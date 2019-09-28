a3287bee42e0ca28014759b92cbc115f39898a25
Carl Magnuson <magnuson.carl@gmail.com>
2019-09-28 11:20:47 -0400

    Allow a message for DataHandlingException

--- src/main/java/com/mtecresults/mylapstcpserver/domain/DataHandlingException.java
+++ src/main/java/com/mtecresults/mylapstcpserver/domain/DataHandlingException.java
@@ -1,4 +1,12 @@
 package com.mtecresults.mylapstcpserver.domain;
 
 public class DataHandlingException extends Exception {
+
+    public DataHandlingException(String message){
+        super(message);
+    }
+
+    public DataHandlingException(String message, Throwable cause){
+        super(message, cause);
+    }
 }
